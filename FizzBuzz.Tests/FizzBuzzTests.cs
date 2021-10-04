0fff849c67be779b71bdbb60fc556857731a96c3
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-04 15:39:40 +0200

    Added another naive test for the case where we print N

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -12,5 +12,12 @@ namespace FizzBuzz.Tests
             FizzBuzzService fizzBuzz = new FizzBuzzService();
             Assert.Equal("7", fizzBuzz.Print(7));
         }
+
+        [Fact]
+        public void ShouldPrintNumber2()
+        {
+            FizzBuzzService fizzBuzz = new FizzBuzzService();
+            Assert.Equal("8", fizzBuzz.Print(8));
+        }
     }
 }
