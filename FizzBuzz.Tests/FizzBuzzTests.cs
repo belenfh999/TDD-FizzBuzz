5e6ed3a15bc5a98e4777348f0bc06120d296b558
Belén Ferrón Hurtado <belen.ferron@nextail.co>
2021-10-05 08:39:03 +0200

    Added more tests to check functionality

--- FizzBuzz.Tests/FizzBuzzTests.cs
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -14,8 +14,11 @@ namespace FizzBuzz.Tests
         }
 
         [Theory]
+        [InlineData(1)]
+        [InlineData(4)]
         [InlineData(7)]
         [InlineData(8)]
+        [InlineData(11)]
         public void ShouldPrintNumber(int value)
         {
             Assert.Equal($"{value}", _fizzBuzz.Print(value));
@@ -24,6 +27,8 @@ namespace FizzBuzz.Tests
         [Theory]
         [InlineData(3)]
         [InlineData(6)]
+        [InlineData(9)]
+        [InlineData(27)]
         public void ShouldPrintFizz(int value){
             Assert.Equal("Fizz", _fizzBuzz.Print(value));
         }
@@ -31,6 +36,8 @@ namespace FizzBuzz.Tests
         [Theory]
         [InlineData(5)]
         [InlineData(10)]
+        [InlineData(20)]
+        [InlineData(25)]
         public void ShouldPrintBuzz(int value){
             Assert.Equal("Buzz", _fizzBuzz.Print(value));
         }
@@ -38,6 +45,8 @@ namespace FizzBuzz.Tests
         [Theory]
         [InlineData(15)]
         [InlineData(30)]
+        [InlineData(60)]
+        [InlineData(600)]
         public void ShouldPrintFizzBuzz(int value){
             Assert.Equal("FizzBuzz", _fizzBuzz.Print(value));
         }
